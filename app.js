1e11e8904ed755f6d3b8fdb913607922a51e4f4b
hanssguti12 <147953487+hanssguti12@users.noreply.github.com>
2024-06-10 09:25:50 -0400

    tienda online p3

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+// Configuración de la variable del entorno
+ 
+require('dotenv').config()
+
+//estático 
+
+app.use(express.static(path.join(__dirname,'static/stylesheets/')))
+
+
+// Renderiza la página de inicio
+app.get('/dashboard', (req, res)  =>{
+	res.sendFile(path.join(__dirname,'static/stylesheets/index.html'))
+})
+
+
+// Levanta el servidor
+
+app.listen(3000, (req, res) =>{
+	console.log('Iniciando...');
+});
+
